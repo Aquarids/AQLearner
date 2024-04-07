fa28c84e357a83bd5c25dc52d6164947ddd54503
Aquarids <aquarids.moe@gmail.com>
2024-04-07 15:40:22 +0800

    add simple fl

--- fl_test.py
+++ fl_test.py
@@ -12,13 +12,21 @@ from fl.client import Client
 from fl.server import Server, type_regresion, type_classification
 
 class TestFL(unittest.TestCase):
-    class SimpleModel(torch.nn.Module):
+    class SimpleLinearRegressionModel(torch.nn.Module):
         def __init__(self, n_features):
-            super(TestFL.SimpleModel, self).__init__()
+            super(TestFL.SimpleLinearRegressionModel, self).__init__()
             self.fc = torch.nn.Linear(n_features, 1)
         
         def forward(self, x):
             return self.fc(x)
+        
+    class SimpleLinearClassificationModel(torch.nn.Module):
+        def __init__(self, n_features):
+            super(TestFL.SimpleLinearClassificationModel, self).__init__()
+            self.fc = torch.nn.Linear(n_features, 1)
+        
+        def forward(self, x):
+            return torch.sigmoid(self.fc(x))
         
     def splite_data(self, X, y, n_clients, n_rounds):
         # the data should owned by the clinets themselves rather than the server, here just for showing the concept
@@ -53,7 +61,7 @@ class TestFL(unittest.TestCase):
         X, y = torch.tensor(X, dtype=torch.float32), torch.tensor(y, dtype=torch.float32).view(-1, 1)
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1, random_state=20)
 
-        model = TestFL.SimpleModel(n_features=X.shape[1])
+        model = TestFL.SimpleLinearRegressionModel(n_features=X.shape[1])
         criterion = torch.nn.MSELoss()
         optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
@@ -89,6 +97,50 @@ class TestFL(unittest.TestCase):
 
         progress_bar.close()
         print("MSE: ", mse)
+        
+        server.summary()
+
+    def test_fl_classification(self):
+        X, y = sklearn.datasets.load_breast_cancer(return_X_y=True)
+        X, y = torch.tensor(X, dtype=torch.float32), torch.tensor(y, dtype=torch.float32).view(-1, 1)
+        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1, random_state=42)
+
+        model = TestFL.SimpleLinearClassificationModel(n_features=X.shape[1])
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+        n_clients = 10
+        server = Server(model, optimizer, criterion, type=type_classification)
+        clients = [Client(model, criterion, optimizer) for _ in range(n_clients)]
+
+        n_rounds = 10
+        n_batch_size = 100
+        n_iter = 10
+
+        X_train_rounds, y_train_rounds = self.splite_data(X_train, y_train, n_clients, n_rounds)
+        
+        accuracy = []
+        progress_bar = tqdm(range(n_rounds * n_clients), desc="Training progress")
+        for round in range(n_rounds):
+            gradients = []
+            for client_id in range(n_clients):
+                client = clients[client_id]
+                X_train, y_train = X_train_rounds[client_id][round], y_train_rounds[client_id][round]
+                train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_train, y_train), batch_size=n_batch_size, shuffle=True)
+                client.train(train_loader, n_iter)
+                gradients.append(client.get_gradients())
+                progress_bar.update(1)
+            server.aggretate_gradients(gradients)
+
+            test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_test, y_test), batch_size=n_batch_size, shuffle=True)
+            y_pred_value, _ = server.predict(test_loader)
+            y_test_value = []
+            for _, y in test_loader:
+                y_test_value += y.tolist()
+            accuracy.append(Metrics.accuracy(np.array(y_test_value), np.array(y_pred_value)))
+
+        progress_bar.close()
+        print("Accuracy: ", accuracy)
         
         server.summary()
 
