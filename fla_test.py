c007ddfaa52d4cd3c8ec31719c261f4ddb9b23f8
Aquarids <aquarids.moe@gmail.com>
2024-05-13 10:25:57 +0800

    minor adjust

--- fla_test.py
+++ fla_test.py
@@ -210,7 +210,7 @@ class TestModelPoison(unittest.TestCase):
         print("Weight Poison accuracy: ", weight_poison_accuracy)
 
 
-class TestFLA(unittest.TestCase):
+class TestClassificationFLA(unittest.TestCase):
 
     def _init_clients(self, n_clients, n_malicious_client, model_factory_json,
                       attack_type):
@@ -298,7 +298,23 @@ class TestFLA(unittest.TestCase):
             }]
         }
 
-    def _init_dataloader(self):
+    def _exclude_target_dataset(self, dataset, target_class):
+        new_dataset = []
+        for data, label in dataset:
+            if label != target_class:
+                new_dataset.append((data, label))
+        return new_dataset
+
+    def _filter_target_loader(self, loader, target_class):
+        new_dataset = []
+        for data, label in loader.dataset:
+            if label == target_class:
+                new_dataset.append((data, label))
+        return torch.utils.data.DataLoader(new_dataset,
+                                           batch_size=32,
+                                           shuffle=False)
+
+    def _init_dataloader(self, exclude_target_class=-1):
         transform = torchvision.transforms.Compose([
             torchvision.transforms.Resize((28, 28)),
             torchvision.transforms.ToTensor(),
@@ -314,6 +330,12 @@ class TestFLA(unittest.TestCase):
                                                   download=True,
                                                   transform=transform)
 
+        if exclude_target_class != -1:
+            train_dataset = self._exclude_target_dataset(
+                train_dataset, exclude_target_class)
+            test_dataset = self._exclude_target_dataset(
+                test_dataset, exclude_target_class)
+
         train_loader = torch.utils.data.DataLoader(train_dataset,
                                                    batch_size=32,
                                                    shuffle=True)
@@ -322,8 +344,8 @@ class TestFLA(unittest.TestCase):
                                                   shuffle=False)
         return train_loader, test_loader
 
-    def _prepare(self, attack_type):
-        train_loader, test_loader = self._init_dataloader()
+    def _prepare(self, attack_type, exclude_target_class=-1):
+        train_loader, test_loader = self._init_dataloader(exclude_target_class)
 
         n_clients = 10
         n_malicious_client = 2  # assume poisoned client less than normal clients (1/3)
@@ -340,8 +362,70 @@ class TestFLA(unittest.TestCase):
 
         return server, clients, n_rounds
 
+    def train_no_attack_model(self, exclude_target_class=-1):
+        server, clients, n_rounds = self._prepare(attack_type_none,
+                                                  exclude_target_class)
+        controller = FLController(server, clients)
+        controller.train(n_rounds, mode_avg_weight)
+        server.model_metric.summary()
+        return controller
 
-class TestRobustAggr(TestFLA):
+
+class TestRegressionFLA(TestClassificationFLA):
+
+    def _init_dataloader(self):
+        X, y = sklearn.datasets.fetch_california_housing(return_X_y=True)
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+        X, y = torch.tensor(X, dtype=torch.float32), torch.tensor(
+            y, dtype=torch.float32).view(-1, 1)
+        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+            X, y, test_size=0.1, random_state=10)
+
+        train_loader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(X_train, y_train),
+            batch_size=100,
+            shuffle=True)
+
+        test_loader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(X_test, y_test),
+            batch_size=100,
+            shuffle=False)
+        return train_loader, test_loader
+
+    def _model_json(self):
+        return {
+            "model_type": type_regression,
+            "learning_rate": 0.01,
+            "optimizer": "adam",
+            "criterion": "mse",
+            "layers": [{
+                "type": "linear",
+                "in_features": 8,
+                "out_features": 1
+            }]
+        }
+
+    def _prepare(self):
+        train_loader, test_loader = self._init_dataloader()
+
+        n_clients = 5
+        n_malicious_client = 0
+        n_rounds = 1
+        n_iter = 1
+
+        clients = self._init_clients(n_clients, n_malicious_client,
+                                     self._model_json(), attack_type_none)
+        server = self._init_server(self._model_json())
+
+        for i in range(n_clients):
+            clients[i].setDataLoader(train_loader, n_iter)
+        server.setTestLoader(test_loader)
+
+        return server, clients, n_rounds
+
+
+class TestRobustAggr(TestClassificationFLA):
 
     def _init_server(self, model_factory_json):
         model, model_type, optimizer, criterion = ModelFactory().create_model(
@@ -407,7 +491,7 @@ class TestRobustAggr(TestFLA):
         server.model_metric.summary()
 
 
-class TestAnomalyDetection(TestFLA):
+class TestAnomalyDetection(TestClassificationFLA):
 
     def _init_server(self, model_factory_json):
         model, model_type, optimizer, criterion = ModelFactory().create_model(
@@ -432,7 +516,7 @@ class TestAnomalyDetection(TestFLA):
         server.model_metric.summary()
 
 
-class TestMPC(TestFLA):
+class TestMPC(TestClassificationFLA):
 
     def _init_clients(self, n_clients, n_malicious_client, model_factory_json,
                       attack_type):
@@ -463,7 +547,7 @@ class TestMPC(TestFLA):
         server.model_metric.summary()
 
 
-class TestModelInversion(TestFLA):
+class TestModelInversion(TestClassificationFLA):
 
     def display_inverted_images(self, inverted_images, n_images):
         plt.figure(figsize=(20, 4))
@@ -493,86 +577,14 @@ class TestModelInversion(TestFLA):
         self.display_inverted_images(synthetic_inputs, n_samples)
 
 
-class TestLabelInference(TestFLA):
-
-    def _exclude_target_dataset(self, dataset, target_class):
-        new_dataset = []
-        for data, label in dataset:
-            if label != target_class:
-                new_dataset.append((data, label))
-        return new_dataset
-
-    def _filter_target_loader(self, loader, target_class):
-        new_dataset = []
-        for data, label in loader.dataset:
-            if label == target_class:
-                new_dataset.append((data, label))
-        return torch.utils.data.DataLoader(new_dataset,
-                                           batch_size=32,
-                                           shuffle=False)
-
-    def _init_dataloader(self, exclude_target_class):
-        transform = torchvision.transforms.Compose([
-            torchvision.transforms.Resize((28, 28)),
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.5, ), (0.5, ))
-        ])
-
-        train_dataset = torchvision.datasets.MNIST(root='./data',
-                                                   train=True,
-                                                   download=True,
-                                                   transform=transform)
-        test_dataset = torchvision.datasets.MNIST(root='./data',
-                                                  train=False,
-                                                  download=True,
-                                                  transform=transform)
-
-        train_dataset = self._exclude_target_dataset(train_dataset,
-                                                     exclude_target_class)
-        test_dataset = self._exclude_target_dataset(test_dataset,
-                                                    exclude_target_class)
-
-        train_loader = torch.utils.data.DataLoader(train_dataset,
-                                                   batch_size=32,
-                                                   shuffle=True)
-        test_loader = torch.utils.data.DataLoader(test_dataset,
-                                                  batch_size=32,
-                                                  shuffle=False)
-
-        return train_loader, test_loader
-
-    def _prepare(self, attack_type, exclude_target_class):
-        train_loader, test_loader = self._init_dataloader(exclude_target_class)
-
-        n_clients = 5
-        n_malicious_client = 0
-        n_rounds = 1
-        n_iter = 1
-
-        clients = self._init_clients(n_clients, n_malicious_client,
-                                     self._model_json(), attack_type)
-        server = self._init_server(self._model_json())
-
-        for i in range(n_clients):
-            clients[i].setDataLoader(train_loader, n_iter)
-        server.setTestLoader(test_loader)
-
-        return server, clients, n_rounds
-
-    def train_model(self, target_class):
-        server, clients, n_rounds = self._prepare(attack_type_none,
-                                                  target_class)
-        controller = FLController(server, clients)
-        controller.train(n_rounds, mode_avg_weight)
-        server.model_metric.summary()
-        return controller
+class TestLabelInference(TestClassificationFLA):
 
     def test_label_inference(self):
         target_class = 0  # assume we want to detect target class 0 whether exists
         threshold = 0.1
 
-        compared_model = self.train_model(-1)
-        attacked_model = self.train_model(target_class)
+        compared_model = self.train_no_attack_model(-1)
+        attacked_model = self.train_no_attack_model(target_class)
 
         attack_loader = self._filter_target_loader(
             compared_model.server.test_loader, target_class)
@@ -587,58 +599,7 @@ class TestLabelInference(TestFLA):
               attacked_result)
 
 
-class TestFeatureInference(TestFLA):
-
-    def _init_dataloader(self):
-        X, y = sklearn.datasets.fetch_california_housing(return_X_y=True)
-        scaler = StandardScaler()
-        X = scaler.fit_transform(X)
-        X, y = torch.tensor(X, dtype=torch.float32), torch.tensor(
-            y, dtype=torch.float32).view(-1, 1)
-        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
-            X, y, test_size=0.1, random_state=10)
-
-        train_loader = torch.utils.data.DataLoader(
-            torch.utils.data.TensorDataset(X_train, y_train),
-            batch_size=100,
-            shuffle=True)
-
-        test_loader = torch.utils.data.DataLoader(
-            torch.utils.data.TensorDataset(X_test, y_test),
-            batch_size=100,
-            shuffle=False)
-        return train_loader, test_loader
-
-    def _model_json(self):
-        return {
-            "model_type": type_regression,
-            "learning_rate": 0.01,
-            "optimizer": "adam",
-            "criterion": "mse",
-            "layers": [{
-                "type": "linear",
-                "in_features": 8,
-                "out_features": 1
-            }]
-        }
-
-    def _prepare(self):
-        train_loader, test_loader = self._init_dataloader()
-
-        n_clients = 5
-        n_malicious_client = 0
-        n_rounds = 1
-        n_iter = 1
-
-        clients = self._init_clients(n_clients, n_malicious_client,
-                                     self._model_json(), attack_type_none)
-        server = self._init_server(self._model_json())
-
-        for i in range(n_clients):
-            clients[i].setDataLoader(train_loader, n_iter)
-        server.setTestLoader(test_loader)
-
-        return server, clients, n_rounds
+class TestFeatureInference(TestRegressionFLA):
 
     def test_feature_inference(self):
         server, clients, n_rounds = self._prepare()
@@ -659,7 +620,7 @@ class TestFeatureInference(TestFLA):
               result)
 
 
-class TestSampleInference(TestFeatureInference):
+class TestSampleInference(TestRegressionFLA):
 
     def test_sample_inference(self):
         server, clients, n_rounds = self._prepare()
